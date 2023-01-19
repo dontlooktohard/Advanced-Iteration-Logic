34c27de73c535578e2ebfc363f9ff9309f56b074
Your Name01echo <xitamarfx@gmail.com>
2023-01-19 13:58:56 +0200

    first assignment

---
+++ boolean,value.js
@@ -0,0 +1,9 @@
+// Ex1.1 - Yes or No 
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" 
+// string for false.
+const numbers =function(boolean){
+    if (boolean){
+        return 'yes'
+    } else {return 'no'}
+}
+console.log(numbers('3'));
